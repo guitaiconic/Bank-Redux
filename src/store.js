17496652fc6915afd3cb51d3dc412d0a0df05b0e
Franklin Oni-Ossai <oniossaifranklin@gmail.com>
2024-11-24 04:15:40 +0000

    update

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-//CREATING COMBINATION REDUCER
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
-
-const store = createStore(rootReducer);
+//New store Redux
+const store = configureStore({
+  reducer: {
+    account: accountReducer, 
+    customer: customerReducer
+  }
+})
 
 export default store;
